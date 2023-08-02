7abf3c7dfdb4d3d46df4d8805f1202744bbc45d7
Thioalejo <alejo.saraza@gmail.com>
2023-08-02 14:34:26 -0500

    Creacion de proyecto base y primer Crud creacion de ciudades

---
+++ Shopping/Shopping/Data/Entities/Country.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Shopping.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "País")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(50,ErrorMessage ="El campo {0} debe tener máximo {1} caractéres.")]
+        public string Name { get; set; }
+
+
+    }
+}
